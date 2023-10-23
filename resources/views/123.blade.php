0bf1cb4ed7480bb30df73e55d3d662a3d418f079
oleg <moonnoon0909@gmail.com>
2023-10-23 17:00:18 +0300

    3e4e

---
+++ resources/views/123.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.base')
+@section('contComponents')
+    @include('layouts.contComponents.revolution')
+    @include('layouts.contComponents.welcome')
+    @include('layouts.contComponents.gallery')
+    @include('layouts.contComponents.video')
+    @include('layouts.contComponents.timeline')
+    @include('layouts.contComponents.about')
+    @include('layouts.contComponents.process')
+    @include('layouts.contComponents.team')
+@endsection
+
+
+
+
+

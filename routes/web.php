8ec4e1338843ef84d7cae9e1c6b43ef0dc011c81
oleg <moonnoon0909@gmail.com>
2023-10-25 18:14:33 +0300

    3e4e

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('123');
+});
+Route::get('/', function () {
+    return view('kickMain');
+});
+Route::get('/slider', function () {
+    return view('adminka/admPages/sliderApp');
 });
